4ac5b2fdd8017039220b137f59430dd23fd60508
Evgen <yevgenchikk@mail.ru>
2023-05-17 22:48:33 +0300

    fix: format mouse position

--- src/handlers/mouseHandler.ts
+++ src/handlers/mouseHandler.ts
@@ -9,7 +9,7 @@ const getCoordinate = async () => {
 const mouseHandler = async (command: string, ws: WebSocket) => {
   const { x, y } = await getCoordinate();
 
-  ws.send(`mouse_position${x},${y}`);
+  ws.send(`mouse_position ${x},${y}`);
 };
 
 export default mouseHandler;
