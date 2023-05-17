4dd7652db7731c1f5e443ee5d51921bf897e9e39
Evgen <yevgenchikk@mail.ru>
2023-05-17 22:41:45 +0300

    feat: add mouse position response

---
+++ src/handlers/mouseHandler.ts
@@ -0,0 +1,15 @@
+import { WebSocket } from 'ws';
+import { mouse } from '@nut-tree/nut-js';
+
+const getCoordinate = async () => {
+  console.log(mouse.getPosition());
+  return mouse.getPosition();
+};
+
+const mouseHandler = async (command: string, ws: WebSocket) => {
+  const { x, y } = await getCoordinate();
+
+  ws.send(`mouse_position${x},${y}`);
+};
+
+export default mouseHandler;
--- src/http_server/index.ts
+++ src/http_server/index.ts
@@ -2,6 +2,7 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as http from 'http';
 import { AddressInfo, WebSocketServer } from 'ws';
+import commandController from '../controller/commandHandler.js';
 
 export const httpServer = http.createServer((req, res) => {
   const dirname = path.resolve(path.dirname(''));
@@ -26,23 +27,20 @@ const showParams = ({ port, address }: AddressInfo) => {
   console.log(`Websocket started on adress: ${address} and port: ${port}`);
 };
 
-const wss = new WebSocketServer({ host: '127.0.0.1', port: 8080 });
+export const wss = new WebSocketServer({ port: 8080 });
 
 wss.on('listening', () => {
   const address = wss.address() as AddressInfo;
   showParams(address);
 });
 
-// showParams(address);
-
 wss.on('connection', function connection(ws) {
   ws.on('error', console.error);
 
-  ws.on('message', function message(data) {
+  ws.on('message', async function message(data) {
     console.log('received: %s', data);
+    commandController(data.toString(), ws);
   });
-
-  ws.send('something');
 });
 
 process.on('SIGINT', () => {
