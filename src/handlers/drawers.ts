1acb31dc775791bc20abe14c2fb3cf7c45477fef
Evgen <yevgenchikk@mail.ru>
2023-05-21 16:32:51 +0300

    feat: add buttons press for drawning

--- src/handlers/drawers.ts
+++ src/handlers/drawers.ts
@@ -1,23 +1,25 @@
 import { WebSocket } from 'ws';
-import { Point, down, left, mouse, right, up } from '@nut-tree/nut-js';
+import { Button, Point, down, left, mouse, right, up } from '@nut-tree/nut-js';
 import { getCoordinate } from '../utils/helper.js';
 
 const squareDrawer = async (command: string, width: number, ws: WebSocket) => {
   const { x, y } = await getCoordinate();
-
+  await mouse.pressButton(Button.LEFT);
   await mouse.move(down(width));
   await mouse.move(right(width));
   await mouse.move(up(width));
   await mouse.move(left(width));
+  await mouse.releaseButton(Button.LEFT);
 };
 
 const rectangularDrawer = async (command: string, width: number, length: number, ws: WebSocket) => {
   const { x, y } = await getCoordinate();
-
+  await mouse.pressButton(Button.LEFT);
   await mouse.move(down(length));
   await mouse.move(right(width));
   await mouse.move(up(length));
   await mouse.move(left(width));
+  await mouse.releaseButton(Button.LEFT);
 };
 
 const circleDrawer = async (command: string, radius: number, ws: WebSocket) => {
